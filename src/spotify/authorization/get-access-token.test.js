85e00f471fea349198aae5ad54d1d1d5fa9ce76c
jonroosevelt <jonathanroosevelt@gmail.com>
2021-09-03 15:35:26 -0300

    adds get-access-token test

--- src/spotify/authorization/get-access-token.test.js
+++ src/spotify/authorization/get-access-token.test.js
@@ -1,15 +1,26 @@
+
 import getAccessToken from "./get-access-token";
+import fetch, {Response} from 'node-fetch';
+
+jest.mock('node-fetch', ()=>jest.fn())
+
+const mockedResponse = (returnBody, status = 200) => ({
+  ok: true,
+  status,
+  json: () => {
+    return returnBody ? returnBody : {}
+  }
+})
 
 describe('getAccessToken',  () => {
-  const expectedResponse = {
-    statusCode: 200,
-    body: {
-      access_token: 'any token',
-      refresh_token: 'any refresh token'
-    }
-  }
-  const response = getAccessToken('any code');
-  it('should return expected response', () => {
-    expect(response).toEqual(expectedResponse)
+  it('should return expected response', async() => {
+    fetch.mockReturnValue(
+      Promise.resolve(
+        mockedResponse({access_token: 'any token'})
+      )
+    )
+    const accessToken = await getAccessToken('any code');
+    const expectedAccessToken = {accessToken: 'any token'}
+    expect(accessToken).toEqual(expectedAccessToken)
   })
 })
