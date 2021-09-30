744e3ca61e69b645e0b3a7ff31bcf46cc3bc230c
jonroosevelt <jonathanroosevelt@gmail.com>
2021-09-30 10:34:57 -0300

    test(youtube-service): adds test to requestUserConsent

--- src/youtube/youtubeService.test.ts
+++ src/youtube/youtubeService.test.ts
@@ -1,5 +1,9 @@
 import { Credentials } from '../lib/interfaces/service';
 import { youtubeClientService } from './youtubeService';
+import { google } from 'googleapis';
+import { OAuth2Client } from 'google-auth-library';
+
+const OAuth2 = google.auth.OAuth2;
 
 type oauth2 = {
   _clientId: string;
@@ -7,22 +11,47 @@ type oauth2 = {
   redirectUri: string;
 };
 
+type GenerateAuthUrl = {
+  access_type: string;
+  scope: [string];
+};
+
 describe('youtubeClientService', () => {
+  beforeAll(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
+  const credentials: Credentials = {
+    web: {
+      clientId: 'any client',
+      clientSecret: 'any secret',
+      redirectUri: 'http://any-redirect-uri',
+    },
+  };
   describe('createOAuthClient', () => {
-    it('shoud return expected OAuth2', async () => {
-      const credentials: Credentials = {
-        web: {
-          clientId: 'any client',
-          clientSecret: 'any secret',
-          redirectUri: 'http://any-redirect-uri',
-        },
-      };
+    it('should return expected OAuth2', async () => {
       const oauth2 = await youtubeClientService.createOAuthClient(credentials);
       const { _clientId, _clientSecret, redirectUri } =
         oauth2 as Partial<oauth2>;
       expect(_clientId).toEqual(credentials.web.clientId);
       expect(_clientSecret).toEqual(credentials.web.clientSecret);
       expect(redirectUri).toEqual(credentials.web.redirectUri);
+    });
+  });
+  describe('requestUserConsent', () => {
+    jest.mock('google-auth-library');
+    const oauthClient = new OAuth2({ ...credentials.web }) as OAuth2Client;
+    jest
+      .spyOn(OAuth2Client.prototype, 'generateAuthUrl')
+      .mockImplementation(jest.fn());
+    const expectedGenerateAuthUrlParams: GenerateAuthUrl = {
+      access_type: 'offline',
+      scope: ['https://www.googleapis.com/auth/youtube'],
+    };
+    it('should generate auth url', () => {
+      youtubeClientService.requestUserConsent(oauthClient);
+      expect(oauthClient.generateAuthUrl).toHaveBeenCalledWith(
+        expectedGenerateAuthUrlParams
+      );
     });
   });
 });
